7167f0f514fe8bed26ebbad0ec848220d3aeb0ec
NevenPolsak <nevenpolsak1@gmail.com>
2021-12-22 11:02:09 +0100

    update
    update

--- src/List.js
+++ src/List.js
@@ -9,7 +9,7 @@ const List = ({ people }) => {
           <article key={id} className="person">
             <img src={image} alt={name} />
             <div>
-              <h4>{name}</h4>
+                 <h4>{name}</h4>
               <p>{age} years</p>
             </div>
           </article>
